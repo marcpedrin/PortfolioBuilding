e0658ee23306d714ce403597250cfff3a7d414ad
marcpedrin <marc.pedrin.84@gmail.com>
2026-09-02 11:15:44 +0530

    greet

--- Activity 1/function.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void greet(char *name){
-    printf("hello, %s!\n",name);
-}
-int main(){
-    printf("Hello world\n");
-    greet("john");
-    return 0;
-}
--- Activity 1/greet.c
+++ Activity 1/greet.c
@@ -1,1 +1,10 @@
 //create a main func and a gree func that takes a namewelcome to your github
+#include<stdio.h>
+void greet(char *name){
+    printf("hello, %s!\n",name);
+}
+int main(){
+    printf("Hello world\n");
+    greet("john");
+    return 0;
+}
